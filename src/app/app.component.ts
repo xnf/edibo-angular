627063bc809d730a753c2e2fc453a79b9868a2c6
user <Tatjana.Solovjova@rtu.lv>
2020-07-16 13:01:46 +0300

    Shiten jja?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+const MAXRANGE = 300;
 
 @Component({
   selector: 'app-root',
+  // view
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+// Controller
 export class AppComponent {
-  title = 'ez';
+
+  // imageCount = 0;
+  isVisible = true;
+  imageSources = [];
+
+  increaseImageCount(){
+    const id = this.generateId();
+    this.imageSources.push(
+      `https://picsum.photos/id/${id}/200/150`
+    )
+    // this.imageCount++;
+  }
+
+  toggle(){
+    this.isVisible = !this.isVisible;
+  }
+  
+  private generateId(){
+    return Math.round(Math.random() * MAXRANGE) + 1;
+  }
+
 }
